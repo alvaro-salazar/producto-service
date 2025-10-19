ad89f2ef1a314ea68821955a7f299a78139f9d41
andres-narvaez <aenc063@gmail.com>
2025-10-19 12:53:15 -0500

    interface of service declaring the methods to use in the implementation

---
+++ src/main/java/co/empresa/productoservice/model/services/IProductoService.java
@@ -0,0 +1,15 @@
+package co.empresa.productoservice.model.services;
+
+import co.empresa.productoservice.model.entities.Producto;
+import java.util.List;
+
+/**
+ * Interface que define los métodos que se pueden realizar sobre la entidad Producto
+ */
+public interface IProductoService {
+    Producto save(Producto producto);
+    void delete(Producto producto);
+    Producto findById(long id);
+    Producto update(Producto producto);
+    List<Producto> findAll();
+}
